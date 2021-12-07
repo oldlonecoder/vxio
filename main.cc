8486a1645d81e025fc76f78872e2007ea2832f67
oldlonecoder <lussier.serge@gmail.com>
2021-12-07 17:39:16 -0500

    update main.cc

--- main.cc
+++ main.cc
@@ -10,10 +10,10 @@ int main()
 {
     rem::init();
     
-    iostr str = "Hello, this is the %svxio %s(%sV%sirtual e%sX%secutable %sI%snstrucyion %sO%sbject%s) %sworld!\n";
-    str << vxio::color::Lime << vxio::color::Aqua << vxio::color::White << vxio::color::Fuchsia << vxio::color::Yellow <<
-    vxio::color::Fuchsia << vxio::color::Yellow << vxio::color::Fuchsia << vxio::color::Yellow << vxio::color::Fuchsia << vxio::color::Yellow <<
-    vxio::color::Fuchsia << vxio::color::Yellow <<vxio::color::Fuchsia << vxio::color::Yellow;
+    iostr str = "Hello, this is the %svxio %s(%sV%sirtual e%sX%secutable %sI%snstruction %sO%sbject%s) %sproject%s!\n";
+    str << vxio::color::Lime << vxio::color::White << vxio::color::Yellow << vxio::color::Aqua << vxio::color::Yellow <<
+    vxio::color::Aqua << vxio::color::Yellow << vxio::color::Aqua << vxio::color::Yellow << vxio::color::Aqua << vxio::color::White <<
+    vxio::color::Lime << vxio::color::White << vxio::color::Reset;
     logger::debug() << str;
     
     logger::clear([](rem& r){
