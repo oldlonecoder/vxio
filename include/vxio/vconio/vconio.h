8964945685eb0ee16dc6800a26105b4f2104db04
oldlonecoder <lussier.serge@gmail.com>
2021-12-09 15:26:11 -0500

    playing with conio stuff

--- include/vxio/vconio/vconio.h
+++ include/vxio/vconio/vconio.h
@@ -1,8 +1,6 @@
 #pragma once
 
 
-
-//#include <vxio/util/iostr.h>
 #include <vxio/util/geometry.h>
 #include <vxio/util/logger.h>
 
--- main.cc
+++ main.cc
@@ -15,6 +15,18 @@ int main()
     vxio::color::Aqua << vxio::color::Yellow << vxio::color::Aqua << vxio::color::Yellow << vxio::color::Aqua << vxio::color::White <<
     vxio::color::Lime << vxio::color::White << vxio::color::Reset;
     logger::debug() << str;
+    str = "╭";
+    
+    
+    logger::info() << " Some data informations:\n" <<
+        vxio::color::White << "Size of std::string :" << vxio::color::Yellow << sizeof(std::string) << "\n...";
+    logger::info() << vxio::color::White << "length of '╭':" << vxio::color::Yellow << str.length() << '\n';
+    
+    void* p = (void*)str.c_str();
+    iostr hx = "| %08b |";
+    uint32_t* t = (uint32_t*)p;
+    hx << *t;
+    logger::info() << vxio::color::White << "uint64_t: " << vxio::color::Yellow << hx() << '\n';
     
     logger::clear([](rem& r){
        std::cout << r.cc();
