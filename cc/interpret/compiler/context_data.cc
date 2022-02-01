e5a303260ec53d8f539d46a028cb4fb0f849d239
oldlonecoder <lussier.serge@gmail.com>
2022-02-01 11:05:42 -0500

    adding new axt {Arithmetic Expr Tree} class - oops

---
+++ cc/interpret/compiler/context_data.cc
@@ -0,0 +1,208 @@
+// SPDX-FileCopyrightText: 2022 <copyright holder> <email>
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include <vxio/interpret/compiler/context_data.h>
+
+
+using namespace vxio::compiler;
+
+std::stack<context_data> context_data::stack;
+
+
+
+bool context_data::operator++()
+{
+    if(cursor == tokens->end())
+        return false;
+    ++cursor;
+    return cursor == tokens->end();
+}
+
+bool context_data::operator++(int)
+{
+    if(cursor== tokens->end())
+        return false;
+    ++cursor;
+    return cursor == tokens->end();
+}
+
+bool context_data::end(token_data::iterator it) const
+{
+    return it >= tokens->end();
+}
+
+
+bool context_data::end() const
+{
+    return cursor == tokens->end();
+}
+
+
+
+///
+//{
+//    cursor = rhs.cursor;
+//...
+//}
+
+std::string context_data::cache()
+{
+    using vxio::color;
+
+    auto token = tokens_cache.begin();
+    iostr str;
+    str << color::White << "cache" << color::NavajoWhite3 << "{" << color::White;
+    if(tokens_cache.empty())
+    {
+        str << color::Yellow <<  "Empty" << color::Reset << "};";
+        return str();
+    }
+    while(token != tokens_cache.end())
+    {
+        str << color::Yellow <<  (*token)->text() << color::Reset;
+        ++token;
+        if(token != tokens_cache.end()) str += ' ';
+    }
+    str << color::NavajoWhite3 << "};";
+    return str();
+}
+/**
+ * @brief --> Advance the internal cached token iterator
+ *
+ * @author &copy;2022 Serge Lussier (lussier.serge@gmail.com); oldlonecoder
+ * @return boolean, true if the iterator is not at the end of the cache, false otherwise.
+ *
+ * @note note.... lol.
+ */
+bool context_data::operator--()
+{
+    if(token_ptr == tokens_cache.end()) return false;
+    ++token_ptr;
+    return token_ptr != tokens_cache.end();
+}
+
+
+bool context_data::operator--(int)
+{
+    if(token_ptr == tokens_cache.end()) return false;
+    ++token_ptr;
+    return token_ptr != tokens_cache.end();
+}
+
+vxio::token_data::pointer context_data::begin_cache()
+{
+    token_ptr = tokens_cache.begin();
+    return token_ptr;
+}
+
+
+std::string context_data::status()
+{
+    iostr str;
+    str << color::White << "context status on rule{%s%s%s}%s%s%s%s" ;
+    std::string rl = r ? r->_id : "null";
+    str  << color::Yellow << rl << color::White
+    << rem::code_text(rem::code::endl) << color::White << cache() << rem::code_text(rem::code::endl)
+    << (end(cursor) ? " end of stream ": cursor->mark());
+
+    return str();
+}
+
+int context_data::push(const context_data& ctx)
+{
+    //logger::debug(src_location) << color::White  << " cursor {push} on " << color::Yellow << ctx.cursor->details(true);
+    context_data::stack.push(ctx);
+    return stack.size();
+}
+
+int context_data::pop(context_data& ctx, bool synchronise)
+{
+    if(stack.empty()) return 0;
+    context_data to_sync = stack.top();
+    //logger::debug(src_location) << color::White  << " cursor in stack on " << color::Yellow << to_sync.cursor->details(true);
+    if(synchronise)
+    {
+        to_sync.cursor = ctx.cursor;
+        to_sync.head = ctx.cursor;
+        to_sync.instruction = ctx.instruction;
+        //...
+    }
+    ctx = to_sync;
+    stack.pop();
+    return stack.size();
+}
+
+
+
+context_data::context_data(const context_data& rhs)
+{
+    blk = rhs.blk;
+    tokens_cache = rhs.tokens_cache;
+    token_ptr = rhs.token_ptr;
+    cursor = rhs.cursor;
+    head = rhs.head;
+    tokens = rhs.tokens;
+    r = rhs.r;
+    instruction = rhs.instruction;
+}
+
+context_data::context_data(context_data && rhs) noexcept
+{
+    blk = rhs.blk;
+    tokens_cache = std::move(rhs.tokens_cache);
+    token_ptr = std::move(rhs.token_ptr);
+    cursor = std::move(rhs.cursor);
+    head = std::move(rhs.head);
+    tokens = rhs.tokens;
+    r = rhs.r;
+    instruction = rhs.instruction;
+}
+
+
+context_data & context_data::operator=(const context_data& rhs)
+{
+    blk = rhs.blk;
+    tokens_cache = rhs.tokens_cache;
+    token_ptr = rhs.token_ptr;
+    cursor = rhs.cursor;
+    head = rhs.head;
+    tokens = rhs.tokens;
+    r = rhs.r;
+    instruction = rhs.instruction;
+    return *this;
+}
+
+context_data & context_data::operator=(context_data && rhs) noexcept
+{
+    blk = rhs.blk;
+    tokens_cache = std::move(rhs.tokens_cache);
+    token_ptr = std::move(rhs.token_ptr);
+    cursor = std::move(rhs.cursor);
+    head = std::move(rhs.head);
+    tokens = rhs.tokens;
+    r = rhs.r;
+    instruction = rhs.instruction;
+    return *this;
+}
+
+
+void context_data::sync(const context_data& rhs)
+{
+    cursor = rhs.cursor;
+}
+
+context_data& context_data::operator << (token_data::iterator it)
+{
+    tokens_cache.emplace_back(&(*it));
+    return *this;
+}
+
+void context_data::restart_sequence()
+{
+    clear_cache();
+    logger::debug(src_funcname) << "cursor on '" << color::Yellow
+    << cursor->text() << color::White
+    << "' to be back on head '" << color::Yellow <<
+    head->text() << color::White << "'";
+    cursor = head;
+}
---
+++ include/vxio/interpret/compiler/context_data.h
@@ -0,0 +1,77 @@
+//
+// Created by oldlonecoder on 21-12-10.
+//
+
+/**********************************************************************************************************************************
+ C opyright 2021 Serge Lussier (oldlonecoder), lussier.serge@gmail.com                                                              *
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *************************************************************************************************************************************/
+
+#pragma once
+
+#include <map>
+#include <vxio/interpret/interpret.h>
+#include <stack>
+#include <vxio/lexer/token_data.h>
+
+
+namespace vxio {
+namespace compiler {
+
+/**
+ * @todo write docs
+ */
+struct context_data
+{
+    bloc* blk = nullptr;
+    token_data::cache   tokens_cache;
+    token_data::pointer token_ptr;
+    token_data::iterator   cursor,head;
+    token_data::collection* tokens = nullptr;
+    const rule* r = nullptr;
+    xio::collection xio_cache;  ///<  Sous reserve;
+    xio* instruction = nullptr;
+
+
+    context_data() = default;
+    context_data(context_data&&) noexcept;
+    context_data(const context_data&);
+
+    bool operator++();
+    bool operator++(int);
+
+    bool operator--();
+    bool operator--(int);
+
+    [[nodiscard]] bool end(token_data::iterator it) const;
+    [[nodiscard]] bool end() const;
+
+    context_data& operator=(context_data&&)noexcept;
+    context_data& operator=(const context_data&);
+    void sync(const context_data& rhs);
+    token_data::pointer begin_cache();
+    std::string status();
+    std::string cache();
+    inline void clear_cache() { tokens_cache.clear(); }
+    static int push(const context_data& ctx);
+    static int pop(context_data& ctx, bool synchronise = false);
+    context_data& operator << (token_data::iterator it);
+    static std::stack<context_data> stack;
+    void restart_sequence();
+};
+
+
+
+}}
+
+
